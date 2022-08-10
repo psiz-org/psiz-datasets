c3eaf185b64f3a209f88c2c896e474f13b008df8
Brett David Roads <brett.roads@gmail.com>
2022-08-10 17:02:18 +0000

    Add repo boilderplate

---
+++ README.md
@@ -0,0 +1,5 @@
+![PsiZ logo](docs/img/full_logo_300.png)
+
+# PsiZ Datasets
+
+To build and overwrite use: `tfds build --overwrite`
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""Setup file.
+
+Retained for legacy compatibility and installing via editable mode.
+
+"""
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
